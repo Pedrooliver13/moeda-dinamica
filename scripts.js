f8840ec9db19d34c6da169d76dcdfc34e4504290
Pedro Oliveira <56042296+Pedrooliver13@users.noreply.github.com>
2020-02-23 00:09:10 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,7 +10,12 @@ const mask = {//vai ficar responsável por formatar , para tal coisa(como dinhei
     return new Intl.NumberFormat('pt-BR' , {//formatando para R$(reais)
       style:'currency',
       currency:'BRL'
-    }).format(value/100)
+    }).format(value/100)//é divido por 100, pq quando usamos o replace e adicionamos um número
+                        //primeita vez vai entrar 1 --> 1,00(vai ser formatado assim)
+                        //por causa do replace, na segunda o primeiro numero vai passar pelo replace
+                        // vai ficar assim 
+                        //1º 1 --> 1,00 --> se transforma 100 por causa do replace mas tem de continuar sendo 1,00
+                        // então matematica basica 100 / 100 = 1
   }
 }
 
